de3809ac4976643ce74d9f76788b1d3a3b79bcae
wesleyl29 <wesleyl29@nycstudents.net>
2025-10-06 22:51:27 +0000

    third commit

--- U1T6_Return/RectangleRunner.java
+++ U1T6_Return/RectangleRunner.java
@@ -1,11 +1,27 @@
 package U1T6_Return;
 
+import java.util.Scanner;
+
 public class RectangleRunner {
     public static void main(String[] args) {
-        Rectangle rect1 = new Rectangle(10,20);
-        rect1.printArea();
-        rect1.printBoxVolume(30.75);
-        rect1.printBoxVolume(40.5);
-        System.out.println("Area = "+rect1.calculateArea()+", Volume = "+rect1.calculateBoxVolume(25.0));
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Enter rectangle 1 length: ");
+        int rect1Length = myScanner.nextInt();
+        System.out.print("Enter rectangle 1 width: ");
+        int rect1Width = myScanner.nextInt();
+        System.out.print("Enter rectangle 1 height: ");
+        double rect1Height = myScanner.nextDouble();
+        System.out.print("Enter rectangle 2 length: ");
+        int rect2Length = myScanner.nextInt();
+        System.out.print("Enter rectangle 2 width: ");
+        int rect2Width = myScanner.nextInt();
+        System.out.print("Enter rectangle 2 height: ");
+        double rect2Height = myScanner.nextDouble();
+
+        // finish the program as described below
+        Rectangle rect1 = new Rectangle(rect1Length,rect1Width);
+        Rectangle rect2 = new Rectangle(rect2Length,rect2Width);
+        System.out.println("Rectangle 1's Area: "+rect1.calculateArea()+", Box Volume: "+rect1.calculateBoxVolume(rect1Height));
+        System.out.println("Rectangle 2's Area: "+rect2.calculateArea()+", Box Volume: "+rect2.calculateBoxVolume(rect2Height));
     }
 }
